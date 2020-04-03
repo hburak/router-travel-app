011a280ac68e06c39ce4415ac488445fa13df332
Halil Burak Cetinkaya <halilburakcetinkaya@halilburak.local>
2020-04-03 03:30:26 +0300

    navigation refactored as an independent component and imported into app.vue

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav id="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </nav>
+</template>
+
+<style scoped>
+#nav {
+  display: flex;
+  align-items: center;
+}
+</style>
