abddc1d7f98903d3a67ab83d2646e6ef7e1e5668
Halil Burak Cetinkaya <halilburakcetinkaya@halilburak.local>
2020-04-04 14:38:10 +0300

    static routes removed. navigation is also with dynamic routes. .path added to router-view to update the app within the same component otherwise the route will work but you would not see the related pages.

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,16 +1,54 @@
+/* eslint-disable prettier/prettier */
 <template>
   <nav id="nav">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
+    <p class="logo">The Vue School Travel App</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">Home</router-link>>
+      </li>
+      <li v-for="destination in destinations" :key="destination.name" class="links">
+        <router-link
+          :to="{ name: 'DestinationDetails', params: { id: destination.id } }"
+        >{{ destination.name }}</router-link>
+      </li>
+    </ul>
   </nav>
 </template>
+
+<script>
+import store from "@/store.js";
+
+export default {
+  data() {
+    return {
+      destinations: store.destinations
+    };
+  }
+};
+</script>
 
 <style scoped>
 #nav {
   display: flex;
-  align-items: center;
+}
+#nav a {
+  color: #2c3e50;
+  text-decoration: none;
+  font-weight: bold;
+}
+.nav-links {
+  display: flex;
+}
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+.links:hover {
+  text-decoration: underline;
+}
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -13,33 +13,6 @@ const routes = [
   /* Lazy Loading the components below 
   with webpacks code splitting feature */
   {
-    path: "/brazil",
-    name: "Brazil",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue")
-  },
-  {
-    path: "/hawaii",
-    name: "hawaii",
-    component: () =>
-      import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue")
-  },
-  {
-    path: "/jamaica",
-    name: "jamaica",
-    component: () =>
-      import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue")
-  },
-  {
-    path: "/panama",
-    name: "panama",
-    component: () =>
-      import(/* webpackChunkName: "panama" */ "../views/Panama.vue")
-  },
-  {
     path: "/details/:id",
     name: "DestinationDetails",
     component: () => import("../views/DestinationDetails.vue")
